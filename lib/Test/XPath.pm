4dc331dccca9ebe12d9bfce3b976625a21ee55db
David E. Wheeler <david@justatheory.com>
2009-08-26 17:02:50 -0700

    First commit
    Just the scaffolding for the module. More to come soon.

---
+++ lib/Test/XPath.pm
@@ -0,0 +1,69 @@
+package Test::XPath;
+
+use strict;
+our $VERSION = '0.01';
+
+
+1;
+__END__
+
+=begin comment
+
+Fake-out Module::Build. Delete if it ever changes to support =head1 headers
+other than all uppercase.
+
+=head1 NAME
+
+Test::XPath - Test XML and HTML using XML::LibXML XPath expressions
+
+=end comment
+
+=head1 Name
+
+Test::XPath - Test XML and HTML using XML::LibXML XPath expressions
+
+=head1 Synopsis
+
+  use Test::More plan => 1;
+  use Test::XPath;
+
+
+
+=head1 Description
+
+
+
+=head1 Support
+
+This module is stored in an open GitHub repository,
+L<http://github.com/theory/test-xpath/tree/>. Feel free to fork and
+contribute!
+
+Please file bug reports at L<http://github.com/theory/test-xpath/issues/>.
+
+=head1 Author
+
+=begin comment
+
+Fake-out Module::Build. Delete if it ever changes to support =head1 headers
+other than all uppercase.
+
+=head1 AUTHOR
+
+=end comment
+
+=over
+
+=item David E. Wheeler <david@kineticode.com>
+
+=back
+
+=head1 Copyright and License
+
+Copyright (c) 2009 David E. Wheeler. Some Rights Reserved.
+
+This module is free software; you can redistribute it and/or modify it under
+the same terms as Perl itself.
+
+=cut
+
